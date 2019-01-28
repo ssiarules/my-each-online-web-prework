e9b0d0380d010b4718573c02c741ed9bb5deda82
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-28 23:56:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,19 +10,20 @@ def my_each(collection) # put argument(s) here
     array 
  end 
 else 
-  puts 
+  puts array 
+end 
 
 
-if block_given?
-    i = 0
+#if block_given?
+    #i = 0
  
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
+   # while i < array.length
+    #  yield(array[i])
+    #  i = i + 1
+   # end
  
-    array
-  else
-    array 
-  end
+   # array
+ # else
+   # array 
+  #end
 end
