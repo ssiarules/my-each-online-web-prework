b8c8f363f9fb0d65363cecd2be54bf0c07fa35fd
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-28 23:52:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+end
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
 end
